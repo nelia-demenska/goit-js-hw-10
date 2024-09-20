d04dca48177c583b0489afa8041413aa43cc668d
nelia-demenska <168294367+nelia-demenska@users.noreply.github.com>
2024-09-20 18:09:48 +0000

    Deploying to gh-pages from @ nelia-demenska/goit-js-hw-10@82d9943bcf954ca9f31e88c4cbe543449ffbc3b7 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i}from"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector(".form");s.addEventListener("submit",function(o){o.preventDefault();const t=parseInt(s.elements.delay.value),m=s.elements.state.value;new Promise((e,n)=>{setTimeout(()=>{m==="fulfilled"?e(t):n(t)},t)}).then(e=>{i.success({title:"✅ Fulfilled",message:`Promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"❌ Rejected",message:`Promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=2-snackbar.js.map
